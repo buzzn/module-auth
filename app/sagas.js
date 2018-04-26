86f2ac26a2bb8aa55f4f7a3d1b2fb1936b0f1a6c
ChipJuggler <scsi.rdx@gmail.com>
2018-04-27 02:12:43 +0300

    initial ping

--- app/sagas.js
+++ app/sagas.js
@@ -1,16 +1,16 @@
-import { call, put, take, select, fork, cancel } from 'redux-saga/effects';
-import { delay } from 'redux-saga';
-import constants from './constants';
-import actions from './actions';
-import api from './api';
+import { call, put, take, select, fork, cancel } from "redux-saga/effects";
+import { delay } from "redux-saga";
+import constants from "./constants";
+import actions from "./actions";
+import api from "./api";
 
 export const getAuth = state => state.auth;
 
 export function* ping({ apiUrl, apiPath, token }) {
   while (true) {
     try {
-      yield delay(1000 * 60);
       yield call(api.ping, { apiUrl, apiPath, token });
+      yield delay(1000 * 60);
     } catch (error) {
       yield put(actions.signOut());
     }
@@ -27,7 +27,12 @@ export default function* authentication() {
       const { login, password } = yield select(getAuth);
 
       try {
-        const res = yield call(api.passwordAuthorize, { login, password, apiUrl, apiPath });
+        const res = yield call(api.passwordAuthorize, {
+          login,
+          password,
+          apiUrl,
+          apiPath
+        });
         token = res.token;
         yield call(api.setAuthTokens, { token });
         yield put(actions.authorizeSuccess(token));
